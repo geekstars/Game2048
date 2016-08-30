d6cb20085d522b939eb0340d7fee571267f40388
Hoàng Minh Thành <hoangminhthanh@hoangs-macbook-pro.local>
2016-08-30 11:59:38 +0700

    Initial Commit

---
+++ Game2048/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Game2048
+//
+//  Created by Hoàng Minh Thành on 8/30/16.
+//  Copyright © 2016 Hoàng Minh Thành. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
